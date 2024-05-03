84026e43a0f9501b23030c0185f0b471ecdb6814
Twister10000 <82016318+Twister10000@users.noreply.github.com>
2024-05-03 07:46:12 +0200

    added some comments

--- Source/VGA_sync.vhd
+++ Source/VGA_sync.vhd
@@ -54,19 +54,19 @@ begin
 				if hor > HFP and hor < HFP + HS then --sync Puls ausgeben
 					hsync <= polH;
 				end if;
-				
+				-- Das Ende von einer Zeile wird geprüft
 				if hor = Htot - 1 then			
 					hor <= 0;
-					ver <= ver + 1;
+					ver <= ver + 1; -- Falls Ende gehen wir auf die nächste Zeile
 				end if;
 				
 				vsync <= not polV;
 				if ver > VFP and ver < VFP + VS then
 					vsync <= polV;
 				end if;
-				
+				-- Das Ende vom Bildschirm wird ÜBERprüft.
 				if ver = Vtot - 1 then
-					ver <= 0;
+					ver <= 0; -- Wir fangen wieder von oben an
 				end if;
 				
 			end if;
