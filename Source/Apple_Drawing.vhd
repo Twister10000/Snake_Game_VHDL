0577125a7c722c7a738782d885236ba101076ab1
Twister10000 <82016318+Twister10000@users.noreply.github.com>
2024-05-09 08:23:53 +0200

    added Basic Input and Output

--- Source/Apple_Drawing.vhd
+++ Source/Apple_Drawing.vhd
@@ -10,25 +10,38 @@ entity Apple_Drawing is
 	port
 	(
 		-- Input ports
-		<name>	: in  <type>;
-		<name>	: in  <type> := <default_value>;
+			xpos_apple     					:	in  	integer range 0 to 1300;   						-- Pixel Pos x Bildbereich
+			ypos_apple     					:	in  	integer range 0 to 1033;    					-- Pixel Pos y Bildbereich
+			Reset										:	in		std_logic;
+			videoOn_apple  					:	in  	std_logic;               							-- 1 = Bildbereich
+			vga_clk									:	in		std_logic;
+			NewFrame_apple					:	in		std_logic;
 
 		-- Inout ports
-		<name>	: inout <type>;
 
 		-- Output ports
-		<name>	: out <type>;
-		<name>	: out <type> := <default_value>
+			Draw_Apple							: out 	std_logic := 	'0'
 	);
 end Apple_Drawing;
-
--- Library Clause(s) (optional)
--- Use Clause(s) (optional)
 
 architecture beh_Apple_Drawing of Apple_Drawing is
 
 	-- Declarations (optional)
-
+	
+		-- Declarations Own Var Types
+			type 				x_pos_arr 					is array (0 to 41/*Nachrechnen*/) of integer range -100 to 1280;
+			type 				y_pos_arr 					is array (0 to 40/*Nachrechnen*/) of integer range -100 to 1024;
+			
+		-- Constants
+			constant		X_range								:		integer	range 0	to 1280		:= 1240;		-- Von wo bis wo darf sich der Balken bewegen
+			constant		Y_range								:		integer	range 0 to 1024		:= 984;
+			
+			
+		-- Declarations Signal
+			signal			Update_Sig											:		std_logic	:= '0';																			--The update signal is responsible for updating the position of the snake. 
+			signal			CLK_ENA_1												:		std_logic := '0';
+			signal 			x_apple													:	x_pos_arr := (others => 0);
+			signal 			y_apple													:	y_pos_arr := (others => 0);
 begin
 
 	-- Process Statement (optional)
