ec39f5052c5d9b5af5f067ca83c054c8b7f87d9f
Twister10000 <82016318+Twister10000@users.noreply.github.com>
2024-05-09 08:16:20 +0200

    Snake_Drawing Instantiation in Game_Main

--- Source/GAME_MAIN.vhd
+++ Source/GAME_MAIN.vhd
@@ -12,6 +12,22 @@ entity Game_Main is
 
 	port
 	(
+		-- Input ports
+			xpos_game     					:	in  	integer range 0 to 1300;   						-- Pixel Pos x Bildbereich
+			ypos_game     					:	in  	integer range 0 to 1033;    					-- Pixel Pos y Bildbereich
+			BTN_LEFT								:	in	 	std_logic;
+			BTN_RIGHT								:	in	 	std_logic;
+			Reset										:	in		std_logic;
+			videoOn_game  					:	in  	std_logic;               							-- 1 = Bildbereich
+			vga_clk									:	in		std_logic;
+			NewFrame_game						:	in		std_logic;	
+
+		-- Inout ports
+
+
+		-- Output ports
+			Draw_Snake_Out							: out 	std_logic := 	'0';
+			Draw_Apple_Out							:	out		std_logic	:=	'0');
 end Game_Main;
 
 architecture beh_Game_Main of Game_Main is
@@ -24,6 +40,35 @@ architecture beh_Game_Main of Game_Main is
 
 begin
 
+	/*Snake_Drawing Instantiation*/
+		
+		Snake_Drawing	:	entity	work.snake_Drawing
+			port map (
+					xpos_snake     					=>	xpos_game,
+					ypos_snake     					=>	ypos_game,
+					BTN_LEFT								=>	BTN_LEFT,
+					BTN_RIGHT								=>	BTN_RIGHT,
+					videoOn_snake  					=>	videoOn_game,
+					vga_clk									=>	vga_clk,
+					NewFrame_snake					=>	NewFrame_game,
+					Draw_Snake							=>	Draw_Snake_In,
+					Reset										=>	Reset);
+					
+					
+					
+				Game_Main : process(all)
+				
+				begin
+						
+						if rising_edge(vga_clk)	then
+							
+							Draw_Apple_Out <= Draw_Apple_In;
+							Draw_Snake_Out <=	Draw_Snake_In;
+							
+							
+						end if;
+						
+				end process Game_Main;
 	-- Process Statement (optional)
 
 	-- Concurrent Procedure Call (optional)
