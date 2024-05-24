9504dc2fe5830dbed6bdb5ea2a3cc64f33589fc4
Twister10000 <82016318+Twister10000@users.noreply.github.com>
2024-05-24 17:12:15 +0200

    VGA_SNAKE_TOP Clean UP + Comments

--- Source/VGA_SNAKE_TOP.vhd
+++ Source/VGA_SNAKE_TOP.vhd
@@ -12,10 +12,10 @@ entity VGA_SNAKE_TOP is
 	port
 	(
 		-- Input ports
-			clk  					: 		in   	std_logic;                 -- vga clock 
-			Reset    			: 		in   	std_logic;                 -- 1 = Reset
-			BTN_LEFT    	:			in 		std_logic;
-			BTN_RIGHT			:			in 		std_logic;
+			clk  					: 		in   	std_logic;                 										-- vga clock 
+			Reset    			: 		in   	std_logic;                 										-- 1 = Reset Button 00
+			BTN_LEFT    	:			in 		std_logic;								 										-- Button 02	
+			BTN_RIGHT			:			in 		std_logic;																		-- Button 01
 
 		-- Inout ports
 
@@ -23,9 +23,9 @@ entity VGA_SNAKE_TOP is
 		-- Output ports
 		hsync_top    	: 		out  	std_logic;                 											-- Hsync Monitor
 		vsync_top    	: 		out		std_logic;                 											-- Vsync Monitor
-		R							:			out		std_logic_vector(3 downto 0);
-		G							:			out		std_logic_vector(3 downto 0);
-		B							:			out		std_logic_vector(3 downto 0)
+		R							:			out		std_logic_vector(3 downto 0);										-- 4-Bit Vektor VGA RED
+		G							:			out		std_logic_vector(3 downto 0);										-- 4-Bit Vektor VGA GREEN
+		B							:			out		std_logic_vector(3 downto 0)										-- 4-Bit Vektor VGA BLUE
 
 	);
 end VGA_SNAKE_TOP;
@@ -33,29 +33,30 @@ end VGA_SNAKE_TOP;
 
 architecture VGA_DEMO_TOP of VGA_SNAKE_TOP is
 		-- Declarations Constant
-		constant MAX_Y : integer := 6; -- number of Pixels in y dir
-		constant MAX_X : integer := 6; -- number of Pixels in x dir
-		constant PIC_MAX_X  : integer := 256;                 -- Bildgroesse in x Richtung (horizontal)
-    constant PIC_MAX_Y  : integer := 272;                 -- Bildgroesse in y Richtung (vertikal)
+		constant 		MAX_Y 										:		integer	range	0	to	300 	:= 6; 													-- number of Pixels in y dir
+		constant 		MAX_X 										:		integer	range	0	to	300 	:= 6; 													-- number of Pixels in x dir
+		constant 		PIC_MAX_X  								:		integer	range	0	to	300 	:= 256;                 				-- Bildgroesse in x Richtung (horizontal)
+    constant 		PIC_MAX_Y  								:		integer	range	0	to	300 	:= 272;                 				-- Bildgroesse in y Richtung (vertikal)
 		-- Declarations Own Var Types
-		type graphicsRGB is array (0 to MAX_x-1,0 to MAX_Y-1) of std_logic_vector(3 downto 0);
+		type graphicsRGB is array (0 to MAX_x-1,0 to MAX_Y-1) of std_logic_vector(3 downto 0);							-- 
 
 		-- Declarations Signal
-		signal			xpos_top     							:  	integer range 0 to 1300;   						-- Pixel Pos x Bildbereich
-		signal			ypos_top     							:  	integer range 0 to 1033;    					-- Pixel Pos y Bildbereich	
-		signal			videoOn_top  							:  	std_logic;               							-- 1 = Bildbereich
-		signal			vga_clk										:		std_logic;
-		signal			NewFrame_top							:		std_logic;
-		signal			Game_On										:		std_logic := '0';
-		signal			Draw_Snake								:		std_logic	:= 	'0';
-		signal			Draw_Snake_Zero						:		std_logic	:= 	'0';
-		signal			Draw_Apple								:		std_logic	:=	'0';
-		signal			BTN_RESET_SYNC									:		std_logic_vector (1 downto 0);    
-
-    signal   x_start    : integer := 620;                  -- Bildkoordinate x = 50 
-    signal   y_start    : integer := 512;                  -- Bildkoordinate y = 10 
-    signal   Adr        : std_logic_vector(14 downto 0);  -- Adressen 
-    signal   q          : std_logic_vector(13 downto 0);  -- Daten  
+		signal			xpos_top     							:  	integer range 0 to	1300 	:= 0;   												-- Pixel Pos x Bildbereich
+		signal			ypos_top     							:  	integer range 0 to	1033 	:= 0;    												-- Pixel Pos y Bildbereich
+    signal   		x_start								    : 	integer range	0	to	800 	:= 620;													-- Bildkoordinate x = 50 
+    signal   		y_start								    : 	integer	range	0	to	700 	:= 512;													-- Bildkoordinate y = 10
+																									
+		signal			videoOn_top  							:  	std_logic :=	'0';              													-- 1 = Bildbereich
+		signal			vga_clk										:		std_logic :=	'0';																				-- Global Clock
+		signal			NewFrame_top							:		std_logic :=	'0';																				-- 1 = NewFrame on VGA	
+		signal			Game_On										:		std_logic :=	'0';																				-- Signal for Starting the Snake Game
+		signal			Draw_Snake								:		std_logic	:=	'0';																				-- Signal for Snake-Body Drawing on VGA Output
+		signal			Draw_Snake_Zero						:		std_logic	:=	'0';																				-- Signal for Snake-Head Drawing on VGA Output
+		signal			Draw_Apple								:		std_logic	:=	'0';																				-- Signal for Apple Drawing on VGA Output
+																										
+		signal			BTN_RESET_SYNC						:		std_logic_vector	(1 downto 0);														-- Vektor for Syncing    
+    signal   		Adr								        : 	std_logic_vector	(14 downto 0);													-- Adressen 
+    signal   		q  								        : 	std_logic_vector	(13 downto 0);													-- Daten  
 
 		
 		-- Declarations BoxGraphics
@@ -119,17 +120,19 @@ begin
 					Reset											=>	Reset);
 		/*Grafik Instantiation*/
 		
-		 grafik: entity work.FONTS               -- Name des ROMs: FONTS.vhd
+		 grafik: entity work.FONTS               							-- Name des ROMs: FONTS.vhd
 				port map (
-                clock     => vga_clk,       -- ROM Clock mit vgaclk verbinden
-                address   => Adr,          -- Adresse);
-                q         => q             -- Daten 
+                clock     => vga_clk,     								-- ROM Clock mit vgaclk verbinden
+                address   => Adr,         								-- Adresse);
+                q         => q            								-- Daten 
               );
 	-- Process Statement (optional)
 		Drawing	:	process(all)
+		
 						/*Declarations Variable*/
-						variable cbit: integer range 0 to 15 := 0;
-						variable x1: integer range 0 to 1300 := 0;
+						
+		variable	cbit	: integer range 0 to 15 	:= 0;
+		variable	x1		: integer range 0 to 1300 := 0;
 		begin
 		
 			if rising_edge(vga_clk) then
@@ -143,13 +146,15 @@ begin
 					
 
 				if videoOn_top = '1' then
-					
+				
+					/*GameState FSM*/
 					case	Game_State	is
 						when	Startscreen		=> B <= x"F";
 						when	Game					=> Game_On <= '1';
 						when	Endscreen			=> R <= x"F";	
 						when	others				=> Null;					
 					end case;
+					/*GameState FSM END*/
 					
 					if Game_State = Startscreen then
 						
@@ -158,55 +163,51 @@ begin
 						if ypos_top >= y_start and ypos_top < y_start + PIC_MAX_Y then 
 							x1 := x_start;
 							if xpos_top >= x_start  and xpos_top < x_start + PIC_MAX_X then
-								cbit := 14 - (xpos_top  - x1 );    -- aktuelles Bit berechnen
-								if cbit = 0 then
-										x1 := xpos_top;                -- Zaehler zurücksetzen, um Bitcounter im Bereich 0 - 14 zu halten
-								end if;
-								if q(cbit) = '1' then              -- falls bit = 1:  weiss ausgeben
+								cbit := 14 - (xpos_top  - x1 );    																					-- aktuelles Bit berechnen
+								if cbit = 0 then																					
+										x1 := xpos_top;                																					-- Zaehler zurücksetzen, um Bitcounter im Bereich 0 - 14 zu halten
+								end if;																					
+								if q(cbit) = '1' then              																					-- falls bit = 1:  weiss ausgeben
 										R  <= x"f";
 										G  <= x"f";
 										B  <= x"f";
 								end if;
 							end if;
-							if  xpos_top = x_start + PIC_MAX_X then      -- nach Ende x-Bereich: Adresse erhöhen
+							if  xpos_top = x_start + PIC_MAX_X then      																	-- nach Ende x-Bereich: Adresse erhöhen
 									Adr <= Adr + 1;  
 							end if;
             else
-                Adr <= (others => '0');   -- reset rom address    -- auuserhalb Bild: Adresse resetieren
+                Adr <= (others => '0');   																									-- reset rom address    -- auuserhalb Bild: Adresse resetieren
             end if;
 						
 						/*Grafik Output END*/
-						
-						
-						
+
 						if xpos_top	>= 300 and xpos_top < 300 + MAX_X then
 							if	ypos_top >= 412 and ypos_top	< 412 + MAX_Y	then
 								R <=	BoxGraphics_R(ypos_top - 412, xpos_top - 300);
 								G	<=	BoxGraphics_G(ypos_top - 412, xpos_top - 300);
 								B	<=	BoxGraphics_B(ypos_top - 412, xpos_top - 300);
-							end if;
-						
+							end if;						
 						end if;
-						
 					end if;
-					
+
+					/*VGA OUTPUT Schlange und Apfel*/
 					
 					if xpos_top = 640 then
 						R <= x"F";
 					end if;
 					if Game_On	= '1' then
-						if Draw_Snake = '1' then -- Schlange zeichnen
+						if Draw_Snake = '1' then 																													-- Schlange zeichnen
 							G <= x"F";
 						end if;
-						if Draw_Snake_Zero = '1' then -- Schlange zeichnen
+						if Draw_Snake_Zero = '1' then 																										-- Schlange zeichnen
 							G <= x"F";
 						end if;
 						if	Draw_Apple	= '1'	then
 							R <= x"F";
 						end if;
 					end if;
-					
-					
+										
 					if ypos_top	= 512 then
 						B	<= x"F";
 						R <= x"8";
@@ -216,20 +217,10 @@ begin
 						G		<= x"F";
 						B		<= x"F";
 					end if;
-				end if;
-			end if;
+					
+					/*VGA OUTPUT Schlange und Apfel END*/
+					
+				end if;		-- VideoOn_top
+			end if; 		-- VGA_CLK
 		end process Drawing;
-		
-	-- Concurrent Procedure Call (optional)
-
-	-- Concurrent Signal Assignment (optional)
-
-	-- Conditional Signal Assignment (optional)
-
-	-- Selected Signal Assignment (optional)
-
-	-- Component Instantiation Statement (optional)
-
-	-- Generate Statement (optional)
-
 end VGA_DEMO_TOP;
