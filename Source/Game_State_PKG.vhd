21b88b02397dc583d40da0cc7353f2fa92cdeec2
Twister10000 <82016318+Twister10000@users.noreply.github.com>
2024-06-07 07:34:35 +0200

    Level of difficulty FSM added

--- Source/Game_State_PKG.vhd
+++ Source/Game_State_PKG.vhd
@@ -13,12 +13,14 @@ package Game_State_PKG is
 
 	-- Type Declaration (optional)
 		type 				Game_FSM						is	(Startscreen, Game, Endscreen);  	-- FSM for Game-State
+		type				Game_Level					is	(Easy,	Medium, Hard);
 	-- Subtype Declaration (optional)
 
 	-- Constant Declaration (optional)
 
 	-- Signal Declaration (optional)
-		signal			Game_State				:	Game_FSM := Startscreen;							-- Signal for current Game_State
+		signal			Game_State				:	Game_FSM 		:= Startscreen;							-- Signal for current Game_State
+		signal			Game_Difficulty		:	Game_Level	:= Easy;
 	-- Component Declaration (optional)
 
 end Game_State_PKG;
